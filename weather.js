248ff6fff05d3e37bf1c29ea83bcff79ee9ca922
landsword <dreike1656@gmail.com>
2019-08-12 17:25:08 +0900

    Weather Check Upload 190812

--- weather.js
+++ weather.js
@@ -10,51 +10,49 @@ locList.forEach(function(el) {
 var weather_now = document.getElementById("weather_now");
 var table_5Days = document.getElementById("weather_5Days");
 var weather_now_view = document.getElementsByClassName("weather_now_view");
-var date = new Date;
-var date_standard = `${date.getFullYear()}-${date.getMonth()}-${date.getDay()} ${date.getHours()}:00:00`;
 
-
-var locData = {};   // city main data
-var locData5Days = {};  // 5days weather json data
-var table_now = function() {
-    for (var count = 0; count < locData5Days.length; count++) {     // find stadard date = count
-        let condition = parseInt(locData5Days[count].dt_txt.slice(-8, -6)) - parseInt(date_standard.slice(-8, -6))
-        if (condition <= 0 && condition >= -3) {
-            break;
-        }
-    }
-    weather_now_view[0].innerHTML = loc.value;
-    let img = document.getElementById("weather_img");
-    img.src = `https://openweathermap.org/img/w/${locData5Days[count].weather[0].icon}.png`;
-    weather_now_view[2].innerHTML = locData5Days[count].dt_txt.slice(5, 13) + " 시 기준";
-    weather_now_view[3].innerHTML = (locData5Days[count].main.temp - 273.15).toFixed(1) + " º";
-    weather_now_view[4].innerHTML = (locData5Days[count].main.temp_min - 273.15).toFixed(1) + " º";
-    weather_now_view[5].innerHTML = (locData5Days[count].main.temp_max - 273.15).toFixed(1) + " º";
-    weather_now_view[6].innerHTML = locData5Days[count].wind.speed + " m/s";
-    weather_now_view[7].innerHTML = locData5Days[count].main.humidity + " %";
-    weather_now_view[8].innerHTML = `경도: ${(locData.coord.lat).toFixed(2)} 위도: ${(locData.coord.lon).toFixed(2)}`;
+Date.prototype.format = function() {
+    let date = new Date;
+    let getyear = date.getFullYear();
+    let getmonth = ((date.getMonth() + 1) < 10) ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
+    let getdate = (date.getDate() < 10) ? "0" + date.getDate() : date.getDate();
+    let gethours = (date.getHours() < 10) ? "0" + date.getHours() : date.getHours();
+    let getminutes = (date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes();
+    
+    let format = `${getyear}-${getmonth}-${getdate} ${gethours}:${getminutes}`;
+    
+    return format;
 }
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${loc.value},kr&appid=f6298de834feb1d272496c63e81b3598`)
+
+
+var data_now = function(data) {
+    weather_now_view[0].innerHTML = loc.value;
+    var img = document.getElementById("weather_img");
+    img.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    weather_now_view[1].innerHTML = data.coord.lat + " " + data.coord.lon;
+    weather_now_view[2].innerHTML = data.main.humidity + "% " + data.wind.speed + "m/s";
+    weather_now_view[3].innerHTML = (data.main.temp - 273.15).toFixed(1) + " °C";
+    weather_now_view[4].innerHTML = "min " + (data.main.temp_min - 273.15).toFixed(1) + " °C " +
+                                    " / max " + (data.main.temp_max - 273.15).toFixed(1) + " °C ";
+    var standard_time = document.getElementById("standard_time");
+    standard_time.innerHTML = (new Date).format();
+
+}
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc.value},kr&appid=f6298de834feb1d272496c63e81b3598`)
 .then(function(resp) {
     return resp.json();
 })
 .then(function(json) {
-    locData = json.city;
-    locData5Days = json.list;
-    console.log(locData);
-    console.log(locData5Days);
-    table_now();
+    data_now(json);
 })
 
 
 var weather_check = function() {    // If select.value is change, call weather data.
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${loc.value},kr&appid=f6298de834feb1d272496c63e81b3598`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${loc.value},kr&appid=f6298de834feb1d272496c63e81b3598`)
     .then(function(resp) {
         return resp.json();
     })
     .then(function(json) {
-        locData = json.city;
-        locData5Days = json.list;
-        table_now();
+        data_now(json);
     })
 }
